313a24766a2ecc0129a7008fc469845f97bf90b0
Hardikb19 <hardikbharunt@gmail.com>
2022-10-19 19:23:10 +0530

    Skeleton for radio button created

---
+++ src/radioButton.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PixelRatio, Pressable, StyleSheet, Text, View } from 'react-native';
+
+import { RadioButtonProps } from './props';
+
+export default function RadioButton({
+}: RadioButtonProps) {
+}
