5309c968ec75563c94637beac701f020f5dbb523
Hardikb19 <43416524+Hardikb19@users.noreply.github.com>
2022-10-26 15:18:43 +0530

    code modified (#4)

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './props';
+export { default as RadioGroup } from './RadioGroup';
+export { default as RadioButton } from './RadioButton';
+
+import RadioButtonsGroup from './RadioGroup';
+export default RadioButtonsGroup;
