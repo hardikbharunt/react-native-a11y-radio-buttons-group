f6fc837def30c64018c6950de9bda9a7516b6244
Hardikb19 <hardikbharunt@gmail.com>
2022-10-26 13:18:37 +0530

    props added

---
+++ src/accessibilityTypes.ts
@@ -0,0 +1,6 @@
+type accessibilityLiveRegionType = 'none' | 'polite' | 'assertive'
+type accessibilityRoleType = 'adjustable' | 'alert' | 'button' | 'checkbox' | 'combobox' | 'header' | 'image' | 'imagebutton' | 'keyboardkey' | 'link' | 'menu' | 'menubar' | 'menuitem' | 'none' | 'progressbar' | 'radio' | 'radiogroup' | 'scrollbar' | 'search' | 'spinbutton' | 'summary' | 'switch' | 'tab' | 'tablist' | 'text' | 'timer' | 'togglebutton' | 'toolbar'
+type accessibilityStateType = 'disabled' | 'selected' | 'checked' | 'busy' | 'expanded'
+type accessibilityValueType = 'min' | 'max' | 'now' | 'test'
+
+export { accessibilityLiveRegionType, accessibilityRoleType, accessibilityStateType, accessibilityValueType }  
--- src/props.ts
+++ src/props.ts
@@ -1,5 +1,53 @@
+// To understand each props usage visit https://reactnative.dev/docs/accessibility
+import { accessibilityLiveRegionType, accessibilityRoleType, accessibilityStateType, accessibilityValueType } from "./accessibilityTypes";
+import { View } from 'react-native';
+
+
+export type AccessibilityProps = {
+    accessible?: boolean;
+    accessibilityLabel?: string;
+    accessibilityLabelledBy?: string;
+    accessibilityHint?: string;
+    accessibilityIgnoresInvertColors?: boolean;
+    accessibilityLiveRegion?: accessibilityLiveRegionType;
+    accessibilityRole?: accessibilityRoleType;
+    accessibilityState?: accessibilityStateType;
+    accessibilityValue?: accessibilityValueType;
+    onAccessibilityTap?: Function;
+    onMagicTap?: Function;
+    // ios specific accessibility options
+    accessibilityLanguage?: string;
+    accessibilityViewIsModal?: boolean
+    accessibilityElementsHidden?: boolean;
+    importantForAccessibility?: boolean;
+    onAccessibilityEscape?: boolean;
+};
+
 export type RadioButtonProps = {
+    id: string;
+    customRadioButtonIcon? : View;
+    borderColor?: string;
+    color?: string;
+    radioButtonStyle?: object;
+    description?: string;
+    descriptionStyle?: object;
+    disabled?: boolean;
+    label?: string;
+    labelStyle?: object;
+    layout?: layoutType;
+    onButtonPress?: (id: string) => void;
+    selected?: boolean;
+    iconSize?: number;
+    accessibilityOptions? : AccessibilityProps
 };
 
 export type RadioGroupProps = {
+    radioButtons: RadioButtonProps[];
+    customRadioGroup? : View;
+    radioGroupStyle?: object;
+    layout?: layoutType;
+    onPress?: (radioButtons: RadioButtonProps[]) => void;
+    accessibilityOptions?: AccessibilityProps;
 };
+
+type layoutType = 'row' | 'column'
