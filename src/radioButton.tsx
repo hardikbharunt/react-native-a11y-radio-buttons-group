d412c7c2297d4801992d5db48293f93643e72a9e
Raksha11 <49094544+Raksha11@users.noreply.github.com>
2022-10-30 10:24:42 +0530

    Radio Group component added (#6)
    * Radio Group component added
    
    * RadioButton props added

--- src/radioButton.tsx
+++ src/radioButton.tsx
@@ -4,5 +4,20 @@ import { PixelRatio, Pressable, StyleSheet, Text, View } from 'react-native';
 import { RadioButtonProps } from './props';
 
 export default function RadioButton({
-}: RadioButtonProps) {
+    id,
+    customRadioButtonIcon,
+    borderColor,
+    color = '#444',
+    radioButtonStyle,
+    description,
+    descriptionStyle,
+    disabled = false,
+    label,
+    labelStyle,
+    layout = 'row',
+    onButtonPress,
+    selected = false,
+    iconSize = 24,
+    accessibilityOptions
+  }: RadioButtonProps) {
 }
